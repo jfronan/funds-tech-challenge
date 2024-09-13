6ac2a2898b66fcca41866f9e6947cc46cce0ba41
jfronan <juanfronan@hotmail.com>
2024-09-13 02:36:55 +0200

    Run create-react-app to generate initial deps template

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ChallengePage from './challenge-page/ChallengePage';
+
+function App() {
+  return (
+    // I'm keeping pre-installed code up to here to keep some useful stuff from create-react-app
+    // Everything inside ChallengePage component is what the challenge is about
+    <ChallengePage />
+  );
+}
+
+export default App;
